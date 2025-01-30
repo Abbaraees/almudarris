e8a1d03ad601381c6d6f4779e57da9d0cfbf85a6
Muhammad Lawal <abbaraees@gmail.com>
2025-01-30 23:21:49 +0100

    feat: implement database migration handling in RootLayout component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,18 +5,29 @@ import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { PaperProvider } from 'react-native-paper'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+
 import theme from '~/constants/Theme';
 import appConfig from '~/stores/AppConfigStore';
+import db from '~/db';
+import migrations from '~/../drizzle/migrations';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(drawer)',
+  initialRouteName: '(tabs)',
 };
 
 const queryClient = new QueryClient()
 
 
 export default function RootLayout() {
+  const { success, error } = useMigrations(db, migrations)
+  if (success) {
+    console.log('Successfully migrated database')
+  } else if (error) {
+    console.error('Failed to migrate database', error)
+  }
+
   useEffect(() => {
     appConfig.initialize()
   }, [])
